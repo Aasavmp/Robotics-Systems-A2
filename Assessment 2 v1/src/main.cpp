31a7a5d498b6ebbc328318125cfa87c83d28a63c
Aasavmp <aasavmp@gmail.com>
2024-03-25 23:24:08 +0000

    Stores waypoints in structure

--- Assessment 2 v1/src/main.cpp
+++ Assessment 2 v1/src/main.cpp
@@ -6,9 +6,16 @@
 // Define number of waypoints
 #define NUM_WAYPOINTS 10
 
+// Define the sensor dead time
+#define SENSOR_DEAD_TIME 1000
+
 // Call the classes
 SearchAlgorithms searchAlgorithms;
 LineSensorClass lineSensor;
+// KinematicsClass kinematicsrun;
+
+// Store the number of waypoints found
+int numWaypointsFound = 0;
 
 // Structure to store coordinates waypoint coordinates for EEPROM storage
 struct waypointCoordinates {
@@ -18,6 +25,12 @@ struct waypointCoordinates {
 
 // Array to store the sensed waypoints
 waypointCoordinates sensedWaypoints[NUM_WAYPOINTS];
+
+// Store the last time the waypoint was sensed
+unsigned long lastWaypointTime = 0;
+
+// Define functions
+void storeWaypoints();
 
 void setup() {
 
@@ -38,3 +51,18 @@ void loop() {
 }
 
 // Function to store the sensed waypoints when the robot is in the search pattern
+void storeWaypoints() {
+
+  // if statement to store data if the line sensor detects a waypoint
+  if (lineSensor.LineDetected() && numWaypointsFound < NUM_WAYPOINTS && millis() - lastWaypointTime > SENSOR_DEAD_TIME) {
+
+    // Store the time the waypoint was sensed
+    lastWaypointTime = millis();
+
+    // Store the waypoint coordinates
+    // sensedWaypoints[numWaypointsFound].x = kinematicsrun.x;
+    // sensedWaypoints[numWaypointsFound].y = kinematicsrun.y;
+
+  };
+
+}
