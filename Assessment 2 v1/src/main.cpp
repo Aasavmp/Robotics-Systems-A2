a835170c8db46810397bf767919213c499b818cd
fpant1 <119616118+fpant1@users.noreply.github.com>
2024-03-26 15:51:07 +0000

    integrated kinematics and encoders

--- Assessment 2 v1/src/main.cpp
+++ Assessment 2 v1/src/main.cpp
@@ -2,9 +2,15 @@
 #include <EEPROM.h>
 #include "SearchAlgorithms.h"
 #include "linesensor.h"
+#include "encoders.h"
+#include "kinematics.h"
 
 // Define number of waypoints
 #define NUM_WAYPOINTS 10
+
+// define timestamps
+#define kinematics_time_interval 20
+unsigned long controltimestamp;
 
 // Define the sensor dead time
 #define SENSOR_DEAD_TIME 1000
@@ -12,6 +18,7 @@
 // Call the classes
 SearchAlgorithms searchAlgorithms;
 LineSensorClass lineSensor;
+Kinematics_c kinematicsrun;
 // KinematicsClass kinematicsrun;
 
 // Store the number of waypoints found
@@ -33,20 +40,39 @@ unsigned long lastWaypointTime = 0;
 void storeWaypoints();
 
 void setup() {
-
+  
   // Run the line sensor initialisation
   lineSensor.init();
+  kinematicsrun.initialise();
 
   // Start the serial monitor
   Serial.begin(9600);
   delay(1000);
   Serial.println("***RESET***");
   delay(2000);
+
+  controltimestamp = millis();
   
 }
 
 void loop() {
+
+  unsigned long currentMillis = millis();
+
+
+
+      // Update kinematics every 20 ms
+  if (currentMillis - controltimestamp >= kinematics_time_interval) {
+    controltimestamp = currentMillis;
+    // Call the updateKinematics method of the Kinematics instance
+    kinematicsrun.update(count_leftenc, count_rightenc);
+    // x, y, theta_1, total_distance = updateKinematics();
+  }
   
+
+
+
+
 
 }
 
