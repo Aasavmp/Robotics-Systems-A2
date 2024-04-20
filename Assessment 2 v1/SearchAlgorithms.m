77394f5c4d884c53c7fd5f02cb9632b19abd7f22
Aasavmp <aasavmp@gmail.com>
2024-04-20 19:40:10 +0100

    Run 2 done

--- Assessment 2 v1/SearchAlgorithms.m
+++ Assessment 2 v1/SearchAlgorithms.m
@@ -2,9 +2,9 @@ clear all
 clc
 
 %% Resoluion, Amplitude and Time period
-resolution = 100;
+resolution = 1000;
 amplitude = 100;
-wavelength = 50;
+wavelength = 200;
 
 %% x and y boundaries
 x_min = 0;
@@ -21,6 +21,7 @@ y = amplitude * sin(x*((2*pi)/wavelength));
 % Plot x vs y for sinusoidal
 figure
 plot(x,y)
+hold on
 
 %% Get y values for square wave
 y = [];
@@ -36,8 +37,7 @@ for x_i = x
 end
 
 % Plot values for the square wave
-figure
-plot(x, y, 'o-')
+plot(x, y)
 
 %% Get x and y values for random search
 x = (x_max-x_min)*rand(resolution, 1)+x_min;
